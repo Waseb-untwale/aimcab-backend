0583604e5208c98a11411d959887aafdfb6e336b
Waseb Untwale <93330099+Waseb-untwale@users.noreply.github.com>
2025-06-19 17:04:54 +0530

    authUser --commit

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const sequelize = require('../config/db');
 const nodemailer = require("nodemailer");
 require("dotenv").config();
